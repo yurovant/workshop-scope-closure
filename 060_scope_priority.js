95d818fedf5f54a292df2ea973fade91e93a417a
Anton Yurovskykh <anton.yurovskykh@gmail.com>
2019-03-12 20:45:06 +0200

    Update and rename 080-scope-priority.js to 060_scope_priority.js

---
+++ 060_scope_priority.js
@@ -0,0 +1,12 @@
+// Local variables have priority over global variables
+
+var animal1 = "dog"
+var animal2 = "cat"
+
+function showAnimals() {
+  var animal2 = "fish"
+  console.log(animal1)
+  console.log(animal2)
+}
+
+showAnimals() // -> dog, fish
