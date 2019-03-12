8698235fc2d2689db203d9c9084f038c1d81b882
Anton Yurovskykh <anton.yurovskykh@gmail.com>
2019-03-12 20:26:57 +0200

    Update and rename 070-variable-mismatch.js to 070_variable_mismatch.js

---
+++ 070_variable_mismatch.js
@@ -0,0 +1,16 @@
+// Variable mismatch
+
+var animal = "dog"
+
+function showAnimal() {
+  console.log(animal)
+}
+
+function showFish() {
+  animal = "fish"
+  console.log(animal)
+}
+
+showAnimal()  // -> dog
+showFish()    // -> fish
+showAnimal()   // -> fish
